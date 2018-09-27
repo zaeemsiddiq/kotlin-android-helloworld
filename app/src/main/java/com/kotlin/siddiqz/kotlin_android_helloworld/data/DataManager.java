5c159bf041a356ad4e5e6d015bf3d4cb124dc601
Zaeem Siddiq <zaeem.siddiq@reece.com.au>
2018-09-27 16:31:49 +1000

    updated project structure

---
+++ app/src/main/java/com/kotlin/siddiqz/kotlin_android_helloworld/data/DataManager.java
@@ -0,0 +1,3 @@
+package com.kotlin.siddiqz.kotlin_android_helloworld.data;
+public class DataManager {
+}
---
+++ app/src/main/java/com/kotlin/siddiqz/kotlin_android_helloworld/data/remote/JobService.java
@@ -0,0 +1,5 @@
+package com.kotlin.siddiqz.kotlin_android_helloworld.data.remote;
+
+//GET POST endpoints here
+public interface JobService {
+}
